f7e6b3456818ead9c752c97411b49f99bd44e1a8
Rezastein <rezastein531@gmail.com>
2021-10-26 22:19:23 +0700

    second commit

--- Biodata.py
+++ Biodata.py
@@ -1,5 +1,5 @@
 fullname = str(input("Your name:"))
-nickname = str(input("username:"))
+nickname = str(input("nickname:"))
 NPM = int(input("Your NPM:"))
 birthplace = str(input("Birthplace:"))
 age = int(input("Your age:"))
@@ -8,9 +8,9 @@ address = str(input("Address:"))
 
 print("\nassalamualaikum wr. wb.")
 
-print("\n Let me introduce my self. My Name is ",fullname, "but you can call me ",nickname)
-print("my NPM is ",NPM,". i was born in ",birthplace," and i am ",age," years old.")
-print("iam very glad if you want to invite my house in ",address,". so don't forget to call me before ")
-print("with the number",phonenumber)
+print("\n Let me introduce my self. My Name is ",fullname, "but you can call me",nickname+".")
+print("my NPM is ",str(NPM)+".","i was born in",birthplace," and iam",age," years old.")
+print("iam very glad if you want to invite my house in",address+".","so don't forget to call me before")
+print("with the number",str(phonenumber)+".")
 
 print("\nThank you")
