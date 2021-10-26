5367fd99ae1f6aec79e5e23554fbdd6f51dbbbee
Rezastein <rezastein531@gmail.com>
2021-10-26 21:23:32 +0700

    first commit

---
+++ Biodata.py
@@ -0,0 +1,16 @@
+fullname = str(input("Your name:"))
+nickname = str(input("username:"))
+NPM = int(input("Your NPM:"))
+birthplace = str(input("Birthplace:"))
+age = int(input("Your age:"))
+phonenumber = int(input("Phone number:"))
+address = str(input("Address:"))
+
+print("\nassalamualaikum wr. wb.")
+
+print("\n Let me introduce my self. My Name is ",fullname, "but you can call me ",nickname)
+print("my NPM is ",NPM,". i was born in ",birthplace," and i am ",age," years old.")
+print("iam very glad if you want to invite my house in ",address,". so don't forget to call me before ")
+print("with the number",phonenumber)
+
+print("\nThank you")
---
+++ arraybiodata.py
@@ -0,0 +1,10 @@
+biodata = [str(input("fullname:")),str(input("nickname:")),int(input("NPM:")),str(input("birthplace:")),int(input("age:")),int(input("phonenumber:")), input("address:")]
+
+print("\n assalamualaikum wr. wb.")
+
+print("\n Let me introduce my self. My Name is ",biodata[0], "but you can call me ",biodata[1])
+print("my NPM is ",biodata[2],". i was born in ",biodata[3]," and i am ",biodata[4]," years old.")
+print("iam very glad if you want to invite my house in ",biodata[6],". so don't forget to call me before ")
+print("with the number",biodata[5])
+
+print("\n Thank you")
